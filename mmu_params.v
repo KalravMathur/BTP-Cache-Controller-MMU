738339830f9ef36722c10d2fb261023cc073555e
Akreus29 <akhilsriram2004@gmail.com>
2025-11-24 18:04:20 +0530

    bug in params

--- mmu_params.v
+++ mmu_params.v
@@ -26,14 +26,17 @@
 
 // --- Address Sizes (configurable) ---
 `define ADDR_WIDTH 32
-`define PAGE_SIZE 4096 // 
-`define OFFSET_BITS $clog2(PAGE_SIZE)
+`define PAGE_SIZE 4096 //
+// FIX: Changed $clog to $clog2
+`define OFFSET_BITS $clog2(`PAGE_SIZE)
+// FIX: Added backticks to internal usage for safety
 `define VPN_WIDTH (`ADDR_WIDTH - `OFFSET_BITS)
 `define PFN_WIDTH (`ADDR_WIDTH - `OFFSET_BITS)
 
 // --- TLB Configuration ---
 `define TLB_ENTRIES 4
-`define TLB_PER_BITS $clog2(TLB_ENTRIES)  // ceil(log2(NUM_TLB_ENTRIES))
+// FIX: Added backticks for safety
+`define TLB_PER_BITS $clog2(`TLB_ENTRIES)
 
 // --- MMU Status Codes ---
 `define STATUS_OK 2'b10 // Translation successful
